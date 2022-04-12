762cc3c513bcbcc9148cba576e1266172fbccc04
ehab0barakat <95265260+ehab0barakat@users.noreply.github.com>
2022-04-12 10:07:39 +0200

    Add files via upload

--- js/java.js
+++ js/java.js
@@ -1,17 +1,68 @@
-$(".h").on("keyup",function(){
-    $(".f").children().remove();
-    hello = setTimeout(()=>{
-    return $(".h").val() ? ehab($(".h").val()) :$(".f").children().remove();
-    },500)
-})
+$("#btnGroupDrop1 ").on("click", (e)=>{
+    
+    $(".ss").removeClass("frame"); 
+    
+    $(e.target).parent().parent().parent().find(".ss").addClass("frame");
+    
+});
 
-ehab = (y)=>{
-$.getJSON("http://collage-site.local/wp-json/wp/v2/posts",
-    function (data) {
-        console.log("ehab")
-        data.map((x,a) =>  x["title"]["rendered"].includes(y) ? $(".f").append(`<li style="list-style:none;">${x["title"]["rendered"]}</li>`) 
-        : "there is no result" )
+
+
+
+
+
+$(".nextt").on("click", ()=>{
+    var style = window.getComputedStyle(document.querySelector('.bottom'));
+    var matrix = new WebKitCSSMatrix(style.transform);
+    var left   = matrix.m41 ;
+
+
+    var card = Number.parseInt($(".memo").css("width"));
+    var bottom = Number.parseInt($(".bottom").css("width"));
+    var father = Number.parseInt($(".father").css("width"));
+    
+
+    var w = left + card + 15
+    
+    
+    console.log(left)
+    if( left >= 0 && left < bottom - father ){
+
+        $(".bottom").css("transform", `translateX(${  w }px)`) ;
+        $(".food").css("transform", `translateX(${  -w }px)`) ;
         
     }
-);
-}
+    
+    
+    if ( left < 0  ){
+        $(".bottom").css("transform", `translateX(${  0 }px)`) ;
+        
+    }
+
+
+
+
+})
+
+
+
+$(".prevv").on("click", ()=>{
+    var style = window.getComputedStyle(document.querySelector('.bottom'));
+    var matrix = new WebKitCSSMatrix(style.transform);
+    var left   = matrix.m41 ;
+    var card = Number.parseInt($(".memo").css("width"));
+    var bottom = Number.parseInt($(".bottom").css("width"));
+    var father = Number.parseInt($(".father").css("width"));
+    
+    var w = left - card - 15
+    
+    if( left > 0 ){
+
+        $(".bottom").css("transform", `translateX(${  w }px)`) ;
+        $(".food").css("transform", `translateX(${  -w }px)`) ;
+
+    }
+
+
+
+})
